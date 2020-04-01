2e1ac050b3dcc8b0b5a40160ca956c4fa2db040a
callor <callor@callor.com>
2020-04-01 11:43:13 +0900

    junit

--- SpMVC24_ModelAttribute/src/main/java/com/biz/models/dao/UserDao.java
+++ SpMVC24_ModelAttribute/src/main/java/com/biz/models/dao/UserDao.java
@@ -4,25 +4,26 @@ import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
 
 import com.biz.models.domain.UsersVO;
 
-@Mapper
+@Repository
 public interface UserDao {
 	
 	@Select("SELECT * FROM tbl_user WHERE userId = #{userId} ")
 	public UsersVO findByUserId(String userId);
 	
-	@Insert(" INSERT INTO tbl_user" + 
-			" (userId," + 
-			" password," + 
-			" userName," + 
-			" userRolle)" + 
-			" VALUES (" + 
-			" #{userId}," + 
-			" #{password}," + 
-			" #{userName}," + 
-			" #{userRolle})")
+//	@Insert(" INSERT INTO tbl_user" + 
+//			" (userId," + 
+//			" password," + 
+//			" userName," + 
+//			" userRolle)" + 
+//			" VALUES (" + 
+//			" #{userId}," + 
+//			" #{password}," + 
+//			" #{userName}," + 
+//			" #{userRolle})")
 	public int insert(UsersVO usersVO);
 	
 	@Delete("DELETE FROM tbl_user WHERE userId = #{userId}" )
